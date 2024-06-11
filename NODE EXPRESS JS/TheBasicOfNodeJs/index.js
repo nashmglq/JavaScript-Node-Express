cf2d6b0604e8284226c1f60a5280f698b3d15f66
nashmglq <nashmglq89@gmail.com>
2024-06-11 12:44:18 +0800

    NPM and basic node

--- NODE EXPRESS JS/TheBasicOfNodeJs/index.js
+++ NODE EXPRESS JS/TheBasicOfNodeJs/index.js
@@ -1,4 +1,22 @@
-console.log("Hello!");
-console.log("THis is the first")
-console.log("Start?")
-process.exit(0)
+// console.log("Hello!");
+// console.log("THis is the first")
+// console.log("Start?")
+// process.exit(0)
+
+
+// const manipulate = require("lodash");
+
+// const arr = [1,2,3]
+
+// console.log(manipulate.chunk(arr))
+
+// const {datas} = require("./repl")
+
+// console.log(datas)
+
+//OR
+
+const {car1, car2} = require("./repl");
+
+console.log(JSON.stringify(car1,null,2))
+console.log(JSON.stringify(car2,undefined,2))
--- NODE EXPRESS JS/TheBasicOfNodeJs/repl.js
+++ NODE EXPRESS JS/TheBasicOfNodeJs/repl.js
@@ -9,3 +9,35 @@ local.on('exit', ()=> {  // "This is like an evnetListner"
     console.log("Ended"); // console.log before the exit
     process.exit();  // if we exit it will do .exit()
 })
+
+// const car = {
+//     brand: "toyota",
+//     model: "supra"
+// }
+
+// module.exports = car;
+
+// const car1 = {
+//     brand: "toyota",
+//     model: "supra"
+// }
+
+// const car2 = {
+//     brand: "Honda",
+//     model: "NSX"
+// }
+
+// exports.datas = {car1 , car2};
+
+//OR
+
+exports.car1 = {
+    brand: "toyota",
+    model: "supra"
+}
+
+exports.car2 = {
+    brand: "Honda",
+    model: "NSX"
+}
+
